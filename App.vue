a4e3de8e09d6dd0618033718321cba65cf04f3d7
summer <2515011987@qq.com>
2022-03-02 17:06:12 +0800

    first commit

---
+++ App.vue
@@ -0,0 +1,23 @@
+<script>
+	export default {
+		onLaunch: function() {
+			console.log('App Launch')
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style>
+	/*每个页面公共css */
+	/* 引入公共样式 */
+	@import './common/uni.css';
+	/* 引入自定义图标库 */
+	@import './common/iconfont.css';
+	/* 引入animate动画库 */
+	@import './common/animate.css';
+</style>
---
+++ unpackage/dist/dev/mp-weixin/pages/home/home.js
@@ -0,0 +1,20 @@
+(global["webpackJsonp"] = global["webpackJsonp"] || []).push([["pages/home/home"],{
+
+/***/ 32:
+/*!*****************************************************************!*\
+  !*** D:/test/fangqiushi/main.js?{"page":"pages%2Fhome%2Fhome"} ***!
+  \*****************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+/* WEBPACK VAR INJECTION */(function(createPage) {__webpack_require__(/*! uni-pages */ 5);
+var _vue = _interopRequireDefault(__webpack_require__(/*! vue */ 3));
+var _home = _interopRequireDefault(__webpack_require__(!(function webpackMissingModule() { var e = new Error("Cannot find module './pages/home/home.vue'"); e.code = 'MODULE_NOT_FOUND'; throw e; }())));function _interopRequireDefault(obj) {return obj && obj.__esModule ? obj : { default: obj };}wx.__webpack_require_UNI_MP_PLUGIN__ = __webpack_require__;
+createPage(_home.default);
+/* WEBPACK VAR INJECTION */}.call(this, __webpack_require__(/*! ./node_modules/@dcloudio/uni-mp-weixin/dist/index.js */ 1)["createPage"]))
+
+/***/ })
+
+},[[32,"common/runtime","common/vendor"]]]);
+//# sourceMappingURL=../../../.sourcemap/mp-weixin/pages/home/home.js.map
